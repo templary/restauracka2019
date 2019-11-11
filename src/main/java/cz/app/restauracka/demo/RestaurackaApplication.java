dc25b1a1350248111a804428bbfa03d7d5807b25
miroslavprokop <mira.prokop@gmail.com>
2019-11-11 17:13:33 +0100

    Spring project.

---
+++ src/main/java/cz/app/restauracka/demo/RestaurackaApplication.java
@@ -0,0 +1,13 @@
+package cz.app.restauracka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurackaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurackaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/app/restauracka/demo/RestaurackaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.app.restauracka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurackaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
